25a1cf354bd425f72911efe56ce3881e4fb3cea9
Kaustubh <kaustubhbiswas001@gmail.com>
2025-12-30 13:36:44 +0530

    feat:hiding sensitive data

--- start-server.bat
+++ start-server.bat
@@ -7,12 +7,19 @@ echo.
 
 cd /d "%~dp0"
 
-set DB_HOST=loan-eligibility-db.cpok88yeay22.ap-south-1.rds.amazonaws.com
-set DB_PORT=5432
-set DB_NAME=loanengine
-set DB_USER=postgres
-set DB_PASSWORD=loaneligibilitydb
-set PORT=8080
+REM Set environment variables (or use existing ones)
+REM To set these permanently, use: setx VARIABLE_NAME "value"
+if not defined DB_HOST set DB_HOST=your-db-host.rds.amazonaws.com
+if not defined DB_PORT set DB_PORT=5432
+if not defined DB_NAME set DB_NAME=loanengine
+if not defined DB_USER set DB_USER=postgres
+if not defined DB_PASSWORD (
+    echo ERROR: DB_PASSWORD environment variable is not set!
+    echo Please set it using: setx DB_PASSWORD "your-password"
+    pause
+    exit /b 1
+)
+if not defined PORT set PORT=8080
 
 echo Database: %DB_HOST%/%DB_NAME%
 echo Port: %PORT%
